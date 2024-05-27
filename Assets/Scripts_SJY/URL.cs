2b662730608ec334f5a7070c3350440710e6411f
테미 <125906814+temmmmmmie@users.noreply.github.com>
2024-05-27 22:26:44 +0900

    share & outline

--- Assets/Scripts_SJY/URL.cs
+++ Assets/Scripts_SJY/URL.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class URL : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private const string subject = "즐거운 피지컬 코딩과 메이커 교육앱 후르츠 루프! 지금 시작해 보세요";
+    private const string body = "https://play.google.com/store/apps/details?id=com.CEREALLAB.FruitsLoop&showAllReviews=true";
 
     public void Share()
     {
-        Application.OpenURL("https://www.naver.com/");
+#if UNITY_ANDROID && !UNITY_EDITOR
+		using (AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent")) 
+		using (AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent")) {
+			intentObject.Call<AndroidJavaObject>("setAction", intentObject.GetStatic<string>("ACTION_SEND"));
+			intentObject.Call<AndroidJavaObject>("setType", "text/plain");
+			intentObject.Call<AndroidJavaObject>("putExtra", intentObject.GetStatic<string>("EXTRA_SUBJECT"), subject);
+			intentObject.Call<AndroidJavaObject>("putExtra", intentObject.GetStatic<string>("EXTRA_TEXT"), body);
+			using (AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+			using (AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity")) 
+			using (AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share Via"))
+			currentActivity.Call("startActivity", jChooser);
+		}
+#endif
+
     }
 
     public void FishingList()
