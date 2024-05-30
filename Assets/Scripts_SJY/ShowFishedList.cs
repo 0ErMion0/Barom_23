d0dca11e20f6c8368b111629653420d536297e26
jwildkj <122768853+jwildkj@users.noreply.github.com>
2024-05-30 16:32:37 +0900

    fix: ShowFishedList 완성
    히ㅏ히 다 고침
    나 어제 왜 밤 샘?
    오늘 다 갈아엎고 새로 했음

--- Assets/Scripts_SJY/ShowFishedList.cs
+++ Assets/Scripts_SJY/ShowFishedList.cs
@@ -6,15 +6,21 @@ using UnityEngine;
 public class ShowFishedList : MonoBehaviour
 {
     public FishedData fishedData = new FishedData();
+    public FishedDataManager FDataMgr;
     public GameObject[] Fish;
     public TextMeshProUGUI[] FishedText;
-    public TextMeshProUGUI text;
+    public TextMeshProUGUI TotalText;
 
 
     //private void Awake()
     //{
     //    FishedText = GetComponents<TextMeshProUGUI>();
     //}
+
+    void Awake()
+    {
+        FDataMgr = GameObject.Find("FishedDataMng").GetComponent<FishedDataManager>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -31,17 +37,25 @@ public class ShowFishedList : MonoBehaviour
     public void FishedResultt()
     {
         //Debug.Log(fishedData.Fished);
-
-            text.text = "피싱 7개 중 " + fishedData.Fished + "회 당하셨습니다!";
+        TotalText.text = "피싱 5개 중 " + FDataMgr.fishedData.Fished + "회 당하셨습니다!";
+        Debug.Log(FDataMgr.fishedData.Fished);
+        Debug.Log("FishedResult 실행");
     }
 
     public void anjgkwl()
     {
-        for (int i = 0; i < fishedData.Fishlist.Length; i++)
+        for (int i = 0; i < FDataMgr.Fishlist.Length; i++)
         {
-            Fish[i].SetActive(fishedData.Fishlist[i]);
-            FishedText[i].color = Color.white;
-            FishedText[2 * i].color = Color.white;
+            Fish[i].SetActive(FDataMgr.Fishlist[i]); //fish image active
+            Debug.Log("Fish SetActive");
+            if (FDataMgr.Fishlist[i])
+            {
+                FishedText[i].color = Color.white;
+                Debug.Log("Chapter Text Active");
+                FishedText[i + 5].color = Color.white;
+                Debug.Log("Text2 Active");
+            }
+
         }
     }
 }
